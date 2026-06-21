bfa0b987870bede40a394a0d19f8328b6ef8f99b
Aadhya Pandillapally <aadhyamsu@gmail.com>
2026-06-21 14:23:00 -0500

    changed last function

--- backend/algorithms/matchmaker.py
+++ backend/algorithms/matchmaker.py
@@ -11,19 +11,22 @@ bullet_embedding structure:[
 job_desc_embedding structure: [0.123, -0.442, ...]
 '''
 
-def calculate_cosine_similarity(job_desc_embedding: list, resume_bullet_embeddings: list) -> float:
+def matchmaker(job_desc_embedding, resume_bullet_embeddings):
     """
-    Calculates the cosine similarity between the job description embedding and the resume bullet embeddings.
+    Ranks the resume bullets based on the cosine similarity between the job description embedding and the resume bullet embeddings.
     
     :param job_desc_embedding: The embedding of the job description.
     :param resume_bullet_embeddings: A dictionary of bullet ids and their embeddings.
     :return: dictionary with the scores and the corresponding bullets.
     """
     job_desc_coords = np.array(job_desc_embedding)
+
     for bullet_embedding in resume_bullet_embeddings:
         coords = np.array(bullet_embedding['embedding'])
         score = np.dot(job_desc_coords, coords) / (np.linalg.norm(job_desc_coords) * np.linalg.norm(coords))
         bullet_embedding["score"] = score  
 
+    # sort by score
+    resume_bullet_embeddings.sort(key=lambda x: x['score'], reverse=True)
     return resume_bullet_embeddings
-    
+
