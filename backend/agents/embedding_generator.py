f25ca0dd4539d4c880d15fbfc3f666ec5c24819a
Aadhya Pandillapally <aadhyamsu@gmail.com>
2026-06-21 14:16:59 -0500

    added embedder

---
+++ backend/agents/embedding_generator.py
@@ -0,0 +1,25 @@
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+client = genai.Client()
+
+result = client.models.embed_content(
+        model="gemini-embedding-001",
+        contents="10x engineer is the best software engineer in the world."
+)
+
+print(result.embeddings)
+
+def generate_embedding(text: str, task_type: str) -> list:
+    """
+    Generate embeddings for a given text using the Gemini API.
+    :param text: The text to generate embeddings for.
+    :param task_type: The type of task, which determines the embedding model to use.
+    :return: A list of embeddings.
+    """
+    result = client.models.embed_content(
+        model="gemini-embedding-001",
+        contents=text
+    )
+    return result.embeddings
