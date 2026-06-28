a881f37b45740706dbbb92895ccaf0a57ab1be85
Aadhya Pandillapally <aadhyamsu@gmail.com>
2026-06-27 20:36:59 -0500

    Added background handling for request

--- frontend/entrypoints/background.ts
+++ frontend/entrypoints/background.ts
@@ -1,3 +1,39 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'GENERATE_RESUME') {
+      browser.storage.local.set({
+        generationState: 'generating'
+      });
+
+      const response = fetch('http://localhost:8000/generate_resume', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ job_description: message.jobDescription }),
+      }).then((result) => {
+        if (result.ok) {
+          result.json().then((data) => {
+            browser.storage.local.set({
+              generationState: {
+                status: 'success',
+                jobDescription: data.jobDescription,
+                resumeData: data.resume,
+                errorMsg: '',
+              }
+            });
+          });
+        } else {
+          browser.storage.local.set({
+            generationState: {
+              status: 'error',
+              jobDescription: message.jobDescription,
+              resumeData: null,
+              errorMsg: 'Failed to generate resume.',
+            }
+          });
+        }
+      });
+    }
+  });
 });
