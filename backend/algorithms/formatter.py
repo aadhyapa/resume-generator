6cab0b562e9d230890f26e5fc305e5cf5f9f6c88
Aadhya Pandillapally <aadhyamsu@gmail.com>
2026-07-11 16:57:32 -0500

    added formatter

---
+++ backend/algorithms/formatter.py
@@ -0,0 +1,25 @@
+
+def formater(edited_bullets, resume):
+    final_resume = {}
+
+    final_resume["header"] = resume["header"]
+
+    final_resume["sections"] = resume["sections"]
+
+    sectioned_bullets = {}
+    for bullet in edited_bullets:
+        if bullet["sub_section_id"] not in sectioned_bullets:
+            sectioned_bullets[ bullet["sub_section_id"]] = []
+        sectioned_bullets[ bullet["sub_section_id"]].append(bullet)
+    
+    for section in resume["sections"]:
+        final_resume[section["section_id"]] = {}
+        final_resume[section["section_id"]]["section_name"] = section["section_name"]
+        final_resume[section["section_id"]]["sub_sections"] = {}
+
+    for sub_section in resume["sub_sections"]:
+        if sub_section["sub_section_id"] in sectioned_bullets:
+            final_resume[sub_section["section_id"]]["sub_sections"][sub_section["sub_section_id"]] = sub_section
+            final_resume[sub_section["section_id"]]["sub_sections"][sub_section["sub_section_id"]]["bullets"] = sectioned_bullets["sub_section_id"]
+
+    return final_resume
